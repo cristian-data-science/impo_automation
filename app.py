27a3b13a78944e9525b5a8b195cd308b5971e67e
Cris <cgutierrez.infor@gmail.com>
2023-04-17 23:36:48 -0400

    starter

--- app.py
+++ app.py
@@ -89,8 +89,14 @@ def show_carga_de_datos(col1, col2):
             # Procesar los IAS aquí
 
             # Leer el archivo IAS de Excel y guardar los datos en un DataFrame
-            df_ias = pd.read_excel(upload_ias)
-            st.write(df_ias)  # Muestra el contenido del DataFrame en la app
+            ias_df = pd.read_excel(upload_ias, header=1)
+            ias_df = ias[['Purchase Order','Product number','Size','Color','Sales Quantity','Sales Amount']]
+            ias_df.rename(columns={'Sales Amount': 'costo_IAS','Purchase Order': 'po'}, inplace=True)
+            ias_df_sum = ias_df.groupby(['po'])[['costo_IAS']].sum()
+            pd.DataFrame(ias_df_sum).reset_index(inplace=True, drop=False)
+            ias_df_sum['po'] = ias_df_sum['po'].astype(int)
+
+            st.write(ias_df_sum)  # Muestra el contenido del DataFrame en la app
 
 def show_insights(col1, col2):
     with col1:
