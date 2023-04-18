6dcfb4e53e23145a742bebcd450b7d48f679a816
Cris <cgutierrez.infor@gmail.com>
2023-04-18 10:50:07 -0400

    starter

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import requests
+import funciones
 
-from funciones import fusionar_pdfs
 from funciones import *
 from streamlit.components.v1 import html
 from streamlit_option_menu import option_menu
