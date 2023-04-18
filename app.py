ba2297a48b5b8f8085c03adacb377868dac3c672
Cris <cgutierrez.infor@gmail.com>
2023-04-17 23:46:57 -0400

    starter

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def show_carga_de_datos(col1, col2):
             pd.DataFrame(ias_df_sum).reset_index(inplace=True, drop=False)
             ias_df_sum['po'] = ias_df_sum['po'].astype(str)
             ias_df_sum['po'] = ias_df_sum['po'].replace(".0","")
-
+            ias_df_sum['po'] = ias_df_sum['po'].apply(lambda x: int(str(x).replace(',', '')))
             ias_df_sum  # Muestra el contenido del DataFrame en la app
 
 def show_insights(col1, col2):
