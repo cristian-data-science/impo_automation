6dc5e4c3692b686b066bc8a8467981800d5ca2fc
Cris <cgutierrez.infor@gmail.com>
2023-04-18 13:42:28 -0400

    add lib

--- app.py
+++ app.py
@@ -128,6 +128,8 @@ def show_insights(col1, col2):
         grid_options_builder.configure_default_column(auto_size_columns= True,filter=True, sortable=True,fit_columns_on_grid_load=True, enable_enterprise_modules=False)
         grid_options = grid_options_builder.build()   
         AgGrid(result, gridOptions=grid_options) 
+
+        ias_df_sum = procesar_ias_excel(upload_ias)
         AgGrid(ias_df_sum, gridOptions=grid_options)
 
 
