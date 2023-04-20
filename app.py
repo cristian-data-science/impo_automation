4a681dd863cc7a2917e28f20fe0fd972577ebb0a
Cris <cgutierrez.infor@gmail.com>
2023-04-20 11:05:43 -0400

    agregación de tabla aperturada por sku

--- app.py
+++ app.py
@@ -88,6 +88,11 @@ def show_carga_de_datos(col1, col2):
     with col1:
         st.sidebar.markdown("Carga de facturas en pdf e international account sales en excel")
         
+    loti2 = 'https://assets7.lottiefiles.com/packages/lf20_lphquaqr.json'
+    lot2 = load_lottie_url(loti2)
+    with col2:
+        st_lottie(lot2, key="loti2")    
+
     with col2:
         st.markdown("### Carga de datos")
         upload_facturas = st.file_uploader("Subir facturas", type=["pdf"], accept_multiple_files=True, key="ias", help="Cargue las facturas en formato PDF.")
