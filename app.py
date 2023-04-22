936ac0f0e1b167df0c00120a433c7a56bc936656
Cris <cgutierrez.infor@gmail.com>
2023-04-22 16:56:40 -0400

    add some features

--- app.py
+++ app.py
@@ -270,6 +270,7 @@ def show_descarga_de_resultados(col1, col2):
     reset_variables()
     invoice_total_lines = None
     total_adjustment_sum = None
+    variables_reset = False
 
     if variables_reset:
 
