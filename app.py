ea5c4a2d578f9afc950b2d1009a05533340328b2
Cris <cgutierrez.infor@gmail.com>
2023-05-24 20:36:18 -0400

    some functions

--- app.py
+++ app.py
@@ -379,8 +379,15 @@ def show_descarga_de_resultados(col1, col2):
 
 
 def show_envio_de_PL_a_EIT(col1, col2):
+
     with col1:
         st.sidebar.markdown("Envío de PL a EIT")
+
+    loti1 = 'https://assets10.lottiefiles.com/private_files/lf30_ig1wfilw.json'
+    lot1 = load_lottie_url(loti1)
+    with col1:
+        st_lottie(lot1, key="loti1", height=200, width=280)
+    
 
     
     st.sidebar.markdown("Envío de PAcking List a EIT")
