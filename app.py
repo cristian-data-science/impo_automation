f3db4bab3636465221f46b5edd69c3b7cb6560a8
Cris <cgutierrez.infor@gmail.com>
2023-06-08 18:02:13 -0400

    Feat: new pro-rateo functionality

--- app.py
+++ app.py
@@ -378,19 +378,14 @@ def show_descarga_de_resultados(col1, col2):
                     # Comprobar si la suma es mayor a 0 y mostrar el mensaje de advertencia
                     if total_adjustment_sum > 0:
                         st.warning('⚠️ Hay handlings fees en las facturas comerciales')
-
-                        if st.button("Aplicar pro-rateo normal"):                          
-                            st.download_button(
-                                label="Descargar Purchase order lines V2",
-                                data=excel_download_data,
-                                file_name="Purchase order lines V2.xlsx",
-                                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                            )
-                        else:
-                            print("NO SE MUESTRA NADA")
-
                     else:
                         st.info(f"""No hay handlings fees asociados a las facturas""")
+
+                    if total_adjustment_sum > 0:
+                        if st.button("Aplicar pro-rateo normal"):                          
+                            st.wrtire(new_df)
+                        else:
+                            print("NO SE MUESTRA NADA")
         
         except FileNotFoundError:
             st.warning("El archivo PDF no se encontró. Cargue un archivo PDF para continuar.")
